e86d05ecacce952e5dd1baffe86309a5bee53934
pavel barovsky <pbarovsky@vk.com>
2024-07-19 15:15:26 +0300

    2.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+
+interface Books {
+  number: number | null;
+  name: string | null;
+  author: string | null;
+}
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'FirstEvoApp';
+  books: Books[] = [
+    {
+      number: 1,
+      name: 'Лес',
+      author: 'Мураками',
+    },
+    {
+      number: 2,
+      name: 'Дом',
+      author: 'Содзи',
+    },
+  ];
+
+  create_book = {
+    number: null,
+    name: null,
+    author: null,
+  };
+
+  createBook() {
+    this.books.push({
+      number: this.create_book.number,
+      name: this.create_book.name,
+      author: this.create_book.author,
+    });
+  }
 }
