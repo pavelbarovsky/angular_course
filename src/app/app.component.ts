5cd72cbb619416bb1709aaef14287395e7491d4d
pavel barovsky <pbarovsky@vk.com>
2024-07-20 20:04:45 +0300

    2.3 v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FirstEvoApp';
+  numbers: number[] = [];
+  randomNumbers: string[] = [];
+  numbersSubscription$: Subscription | null = null; // здесь так чтобы можно было возобновить поток
+  randomNumbersSubscription$: Subscription | null = null; // здесь так чтобы можно было возобновить поток
+
+  constructor() {
+    this.enableNumbersStream();
+    this.enableRandomNumbersStream();
+  }
+
+  enableNumbersStream() {
+    if (!this.numbersSubscription$) {
+      const numbersObservable$ = interval(2000);
+      this.numbersSubscription$ = numbersObservable$
+        .subscribe(num => {this.numbers = [...this.numbers, num];
+      });
+    }
+  }
+
+  enableRandomNumbersStream() {
+    if (!this.randomNumbersSubscription$) {
+      const randomNumbersObservable$ = interval(2000).pipe(
+        map(() => `Random Value: ${Math.floor(Math.random() * 501)}`)
+      );
+      this.randomNumbersSubscription$ = randomNumbersObservable$
+        .subscribe(num => {this.randomNumbers = [...this.randomNumbers, num];
+      });
+    }
+  }
+
+  disableNumbersStream() {
+    if (this.numbersSubscription$) {
+      this.numbersSubscription$.unsubscribe();
+      this.numbersSubscription$ = null;
+    }
+  }
+
+  disableRandomNumbersStream() {
+    if (this.randomNumbersSubscription$) {
+      this.randomNumbersSubscription$.unsubscribe();
+      this.randomNumbersSubscription$ = null;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
